a371277fc53d77e78974bd466560001b602cc5c7
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2025-12-13 22:38:43 +0600

    src/app/chat/layout.jsx

--- src/app/chat/layout.jsx
+++ src/app/chat/layout.jsx
@@ -1,7 +1,17 @@
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export const iframeHeight = "800px";
+
+export const description = "A sidebar with a header and a search form.";
+
 export default function ChatLayout({ children }) {
     return (
-        <div className="">
-            {children}
+        <div className="[--header-height:calc(--spacing(14))]">
+            <SidebarProvider className="flex flex-col">
+                <SiteHeader />
+                {children}
+            </SidebarProvider>
         </div>
     )
 }
